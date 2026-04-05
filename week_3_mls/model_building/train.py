e46cb52d17a4af92620f5508d4dc1c64727a3cc7
PenchalaVineeth <penchala.vineeth3@gmail.com>
2026-04-05 13:39:50 +0000

    first commit

---
+++ week_3_mls/model_building/train.py
@@ -0,0 +1,121 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, classification_report
+import joblib
+import os
+from huggingface_hub import login, HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+import mlflow
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("mlops-training-experiment")
+
+api = HfApi()
+
+Xtrain_path = 'hf://datasets/vineeth32/machine-failure-prediction-data/Xtrain.csv'
+Xtest_path = 'hf://datasets/vineeth32/machine-failure-prediction-data/Xtest.csv'
+ytrain_path = 'hf://datasets/vineeth32/machine-failure-prediction-data/ytrain.csv'
+ytest_path = 'hf://datasets/vineeth32/machine-failure-prediction-data/ytest.csv'
+
+Xtrain = pd.read_csv(Xtrain_path)
+Xtest = pd.read_csv(Xtest_path)
+ytrain = pd.read_csv(ytrain_path)
+ytest = pd.read_csv(ytest_path)
+
+# One-hot encode 'Type' and scale numeric features
+numeric_features = [
+    'Air temperature',
+    'Process temperature',
+    'Rotational speed',
+    'Torque',
+    'Tool wear'
+]
+categorical_features = ['Type']
+
+class_weight = ytrain.value_counts()[0]/ytrain.value_counts()[1]
+
+preprocessor = make_column_transformer(
+    (StandardScaler(), numeric_features),
+    (OneHotEncoder(handle_unknown='ignore'), categorical_features)
+)
+
+xgb_model = xgb.XGBClassifier(scale_pos_weight=class_weight, random_state=42)
+
+param_grid = {
+    'xgbclassifier__n_estimators': [50, 75, 100],
+    'xgbclassifier__max_depth': [2, 3, 4],
+    'xgbclassifier__colsample_bytree': [0.4, 0.5, 0.6],
+    'xgbclassifier__colsample_bylevel': [0.4, 0.5, 0.6],
+    'xgbclassifier__learning_rate': [0.01, 0.05, 0.1],
+    'xgbclassifier__reg_lambda': [0.4, 0.5, 0.6],
+}
+
+model_pipeline = make_pipeline(preprocessor, xgb_model)
+
+with mlflow.start_run():
+  grid_search = GridSearchCV(model_pipeline, param_grid, cv=5, scoring='recall', n_jobs=-1)
+  grid_search.fit(Xtrain, ytrain)
+
+  results = grid_search.cv_results_
+  for i in range(len(results['params'])):
+    param_set = results['params'][i]
+    mean_score = results['mean_test_score'][i]
+    std_score = results['std_test_score'][i]
+
+    with mlflow.start_run(nested=True):
+      mlflow.log_params(param_set)
+      mlflow.log_metric("mean_test_score", mean_score)
+      mlflow.log_metric("std_test_score", std_score)
+
+  mlflow.log_params(grid_search.best_params_)
+
+  best_model = grid_search.best_estimator_
+
+  classification_threshold = 0.45
+  y_pred_train_proba = best_model.predict_proba(Xtrain)[:, 1]
+  y_pred_train = (y_pred_train_proba >= classification_threshold).astype(int)
+
+  y_pred_test_proba = best_model.predict_proba(Xtest)[:, 1]
+  y_pred_test = (y_pred_test_proba >= classification_threshold).astype(int)
+
+  train_report = classification_report(ytrain, y_pred_train, output_dict=True)
+  test_report = classification_report(ytest, y_pred_test, output_dict=True)
+
+  mlflow.log_metrics({
+      "train_accuracy": train_report['accuracy'],
+      "train_precision": train_report['1']['precision'],
+      "train_recall": train_report['1']['recall'],
+      "train_f1-score": train_report['1']['f1-score'],
+      "test_accuracy": test_report['accuracy'],
+      "test_precision": test_report['1']['precision'],
+      "test_recall": test_report['1']['recall'],
+      "test_f1-score": test_report['1']['f1-score']
+    })
+
+  model_path = 'best_machine_failure_model_v1.joblib'
+
+  joblib.dump(best_model, model_path)
+  mlflow.log_artifact(model_path, artifact_path="model")
+  print(f"Model saved as artifact at: {model_path}")
+
+  repo_id = 'vineeth32/machine-failure-prediction-model'
+  repo_type = 'model'
+
+  try:
+      api.repo_info(repo_id=repo_id, repo_type=repo_type)
+      print(f"Model Space '{repo_id}' already exists. Using it.")
+  except RepositoryNotFoundError:
+      print(f"Model Space '{repo_id}' not found. Creating new space...")
+      create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+      print(f"Model Space '{repo_id}' created.")
+
+  api.upload_file(
+      path_or_fileobj='best_machine_failure_model_v1.joblib',
+      path_in_repo="best_machine_failure_model_v1.joblib",
+      repo_id=repo_id,
+      repo_type=repo_type,
+  )
